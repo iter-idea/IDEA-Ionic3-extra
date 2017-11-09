28d1f50cb452d4272e1dd67397c2fea833bd7165
MC <mc@matteocarbone.com>
2017-11-09 18:02:08 +0100

    added url sanitizer to extBrowser

--- idea/extBrowser.service.ts
+++ idea/extBrowser.service.ts
@@ -1,16 +1,20 @@
 import { Injectable } from '@angular/core';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Injectable()
 export class IDEAExtBrowserService {
 
-  constructor() {}
+  constructor(protected sanitizer: DomSanitizer) {}
 
   public openLink(url: string): void {
     // open a link in a safe way so that also in iOS standalone it will work
     let a = document.createElement('a');
-    a.setAttribute('href', url);
+    a.setAttribute('href', <any>this.sanitizer.bypassSecurityTrustUrl(url));
     a.setAttribute('target', '_blank');
     a.click();
     a.remove(); // to clean up
+  }
+  public sanitizeUrl(url: string): SafeUrl {
+    return this.sanitizer.bypassSecurityTrustUrl(url)
   }
 }
