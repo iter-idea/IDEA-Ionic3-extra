4ed8090f209ed288a03db45cea4e26b7c0fb398c
MC <mc@matteocarbone.com>
2017-11-09 19:00:17 +0100

    v0.4.5

--- idea/extBrowser.service.ts
+++ idea/extBrowser.service.ts
@@ -9,7 +9,7 @@ export class IDEAExtBrowserService {
   public openLink(url: string): void {
     // open a link in a safe way so that also in iOS standalone it will work
     let a = document.createElement('a');
-    a.setAttribute('href', <any>this.sanitizer.bypassSecurityTrustUrl(url));
+    a.setAttribute('href', url);
     a.setAttribute('target', '_blank');
     a.click();
     a.remove(); // to clean up
