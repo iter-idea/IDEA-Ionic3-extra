06e9031bf2834a740e3faddc98e622a9d91eaab6
MC <mc@matteocarbone.com>
2018-05-29 17:21:42 +0200

    Error reporting URL (default)

--- assets/configs/idea-config.js
+++ assets/configs/idea-config.js
@@ -9,3 +9,6 @@ const IDEA_AUTH_SHOW_LOGO = true;
 const IDEA_AUTH_VIDEO = true; // if false, load a static image instead
 
 const IDEA_WEBSITE = 'https://iter-idea.com';
+
+const IDEA_ERROR_REPORTING_API_URL =
+  'https://9ssilq3yzh.execute-api.eu-west-2.amazonaws.com/prod/errorReporting';
