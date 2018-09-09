2995a848ee132f3548c802ac89734f39d788132a
MC <mc@matteocarbone.com>
2018-09-09 10:58:43 +0200

    Improved pickers of custom components (2/2)

---
+++ idea/calendar/dateTime.component.ts
@@ -0,0 +1,43 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { ModalController } from 'ionic-angular';
+import { TranslateService } from '@ngx-translate/core';
+
+import { IDEADateUtils } from './dateUtils.service';
+
+@Component({
+  selector: 'IDEADateTimeComponent',
+  templateUrl: 'dateTime.component.html'
+})
+export class IDEADateTimeComponent {
+  @Input() protected label: string;
+  @Input() protected date: Date;
+  @Input() protected timePicker: boolean;
+  @Input() protected toolbarBgColor: string;
+  @Input() protected toolbarColor: string;
+  @Input() protected disabled: boolean;
+  @Output() protected onDateSelected = new EventEmitter<number>();
+
+  constructor(
+    protected modalCtrl: ModalController,
+    protected d: IDEADateUtils,
+    protected t: TranslateService
+  ) {}
+
+  /**
+   * Open the calendar picker to select a date.
+   */
+  protected openCalendarPicker(): void {
+    if(this.disabled) return;
+    let modal = this.modalCtrl.create('idea-calendar', {
+      refDate: this.date, title: this.label, timePicker: this.timePicker,
+      toolbarBgColor: this.toolbarBgColor, toolbarColor: this.toolbarColor
+    });
+    modal.onDidDismiss(date => {
+      if(date !== undefined && date !== null) {
+        if(date == '') this.onDateSelected.emit(null);
+        else this.onDateSelected.emit(date.getTime());
+      }
+    });
+    modal.present();
+  }
+}
